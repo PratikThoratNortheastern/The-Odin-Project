db9fe6a5719949f39e6c09383e85295a5f423820
Pratik Thorat <thorat.p@northeastern.edu>
2024-10-28 14:50:10 -0400

    Finished Assignment for variables and Operators �

---
+++ Foundations/script.js
@@ -0,0 +1,26 @@
+//  ASSIGNMENT
+// 1. Add two numbers together
+console.log(23 + 97);
+
+// 2. Add a sequence of 6 different numbers
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+// 3. Print the value of the following expression: (4 + 6 + 9) / 77
+console.log((4 + 6 + 9) / 77);
+
+// 4. VARIABLES EXERCISE:
+let a = 10;
+console.log(a);
+
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+let max = 57;
+
+let actual = max - 14;
+
+let percentage = (actual / max) * 100;
+
+console.log(percentage);
