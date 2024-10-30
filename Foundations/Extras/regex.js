fc4182a532ce8abfe3218f15227a879a871f5d69
Pratik Thorat <thorat.p@northeastern.edu>
2024-10-30 12:05:16 -0400

    Adds regex file

--- Foundations/Extras/regex.js
+++ Foundations/Extras/regex.js
@@ -1,2 +1,10 @@
 // This workbook is to learn about Regular expressions
 "use strict";
+// Regular expressions are used to perform search operations on a text.
+// Regular expressions have powerful applications such as validation, find and replace and many more.
+/* 
+Syntax of Regex: (/) -> Start and End of a Regular Expression; Example: /cat/<flag>. The flag set certain conditions to perform certain actions. Without the flags the Regular Expression will return us the first result matching the string/expression inside.
+*/
+
+const string =
+  "The fat cat ran down the street. It was searching for a mouse to eat.";
