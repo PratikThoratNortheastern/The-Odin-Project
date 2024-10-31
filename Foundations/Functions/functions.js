a25e88c88b0392981db4ee36909aeb1497beb6c3
Pratik Thorat <thorat.p@northeastern.edu>
2024-10-31 12:20:05 -0400

    Odin Project Overview of functions

---
+++ Foundations/Functions/functions.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// functions are used constantly in Javascript
+
+function favouriteAnimal(animal) {
+  return `${animal} is my favourite animal`;
+}
+
+console.log(favouriteAnimal("Goat"));
+
+function sum(param1, param2) {
+  return param1 + param2;
+}
+
+console.log(sum(1, 4));
