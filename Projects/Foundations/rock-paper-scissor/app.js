254b8fe71d8907c09843eef98074516c90aa6cbb
Pratik Thorat <thorat.p@northeastern.edu>
2024-11-08 14:02:19 -0500

    Defined Logic to get computer and human choice

--- Projects/Foundations/rock-paper-scissor/app.js
+++ Projects/Foundations/rock-paper-scissor/app.js
@@ -1,3 +1,19 @@
 console.log("##########");
 console.log("WELCOME TO ROCK PAPER SCISSORS!");
 console.log("##########");
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+
+  console.log("Computer: " + choices[randomIndex]);
+  return choices[randomIndex];
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt(
+    "Enter any one of the following: Rock, Paper or Scissors?"
+  );
+  console.log("Human Choice: " + humanChoice);
+  return humanChoice;
+}
